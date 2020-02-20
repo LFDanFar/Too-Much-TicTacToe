2daf5d15613f64c4e6f72c46ea5ca8ab4edd99fd
Dan <d9a9n9i9e9l9@hotmail.com>
2020-02-19 23:57:25 -0500

    Fell asleep didn't finish

---
+++ TicTacToeFragments/TicTacToeFragments/app/src/main/java/com/example/tictactoefragments/ListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.tictactoefragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_fragment)
+    }
+}
